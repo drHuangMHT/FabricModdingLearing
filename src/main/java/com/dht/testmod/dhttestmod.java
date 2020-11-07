8a174c09ab94e2577b95f605f1ac48315dab6420
Dr_Huang_MHT <59129019+drHuangMHT@users.noreply.github.com>
2020-11-07 11:28:21 +0800

    更改ModID,并成功启动Mod执行HelloWorld

--- src/main/java/com/dht/testmod/dhttestmod.java
+++ src/main/java/com/dht/testmod/dhttestmod.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package com.dht.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class dhttestmod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
